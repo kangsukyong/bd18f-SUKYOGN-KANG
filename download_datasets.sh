f47ac0ea757d1dc14be3e54f99174864d9a49287
kangsukyong <sykang7078@gmail.com>
2018-11-02 16:38:12 +0900

    Add files via upload
    hw #1 -getting 2 datasets

---
+++ download_datasets.sh
@@ -0,0 +1,2 @@
+wget https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2017-financial-year-provisional/Download-data/annual-enterprise-survey-2017-financial-year-provisional-csv.csv
+wget https://www.stats.govt.nz/assets/Uploads/Business-operations-survey/Business-operations-survey-2017/Download-data/business-operations-survey-2017-business-practices-csv-tables.csv
